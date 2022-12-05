0ccd0e1fc637d0c629bba5b40d8fa6dfd6275e56
gastonsartori <gaston.sartori@mi.unc.edu.ar>
2022-12-05 20:58:47 -0300

    get and vis point clouds

---
+++ carla_save_pointcloud_bin.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2020 Computer Vision Center (CVC) at the Universitat Autonoma de
+# Barcelona (UAB).
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+"""Open3D Lidar visuialization example for CARLA"""
+
+import glob
+import os
+import sys
+import argparse
+import time
+from datetime import datetime
+import random
+import numpy as np
+from matplotlib import cm
+import open3d as o3d
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+def lidar_callback(point_cloud):
+    """Prepares a point cloud with intensity
+    colors ready to be consumed by Open3D"""
+    data = np.copy(np.frombuffer(point_cloud.raw_data, dtype=np.dtype('f4')))
+    data = np.reshape(data, (int(data.shape[0] / 4), 4))
+
+    data.tofile('./point_clouds/%.6d.bin' % point_cloud.frame)
+    print('point cloud %.6d.bin guardada' % point_cloud.frame)
+
+def generate_lidar_bp(arg, world, blueprint_library, delta):
+    """Generates a CARLA blueprint based on the script parameters"""
+
+    lidar_bp = blueprint_library.find('sensor.lidar.ray_cast')
+    if arg.no_noise:
+        lidar_bp.set_attribute('dropoff_general_rate', '0.0')
+        lidar_bp.set_attribute('dropoff_intensity_limit', '1.0')
+        lidar_bp.set_attribute('dropoff_zero_intensity', '0.0')
+    else:
+        lidar_bp.set_attribute('noise_stddev', '0.2')
+
+    lidar_bp.set_attribute('upper_fov', str(arg.upper_fov))
+    lidar_bp.set_attribute('lower_fov', str(arg.lower_fov))
+    lidar_bp.set_attribute('channels', str(arg.channels))
+    lidar_bp.set_attribute('range', str(arg.range))
+    lidar_bp.set_attribute('rotation_frequency', str(1.0 / delta))
+    lidar_bp.set_attribute('points_per_second', str(arg.points_per_second))
+    return lidar_bp
+
+
+def main(arg):
+    """Main function of the script"""
+    client = carla.Client(arg.host, arg.port)
+    client.set_timeout(2.0)
+    world = client.get_world()
+
+    try:
+        original_settings = world.get_settings()
+        settings = world.get_settings()
+
+        delta = 0.05
+
+        settings.fixed_delta_seconds = delta
+        settings.synchronous_mode = True
+        settings.no_rendering_mode = arg.no_rendering
+        world.apply_settings(settings)
+
+        blueprint_library = world.get_blueprint_library()
+
+        lidar_bp = generate_lidar_bp(arg, world, blueprint_library, delta)
+
+        #(x=150.64, y=306.46, z=1.5)
+        #(X=15381.000000,Y=30022.000000,Z=36.000000)
+        #lidar_transform = carla.Transform(carla.Location(arg.x, arg.y, arg.z), 
+        lidar_transform = carla.Transform(carla.Location(0.0, 0.0, 1.5), 
+                            carla.Rotation(pitch=0.0, yaw=0.0, roll=0.0))
+
+
+        lidar = world.spawn_actor(lidar_bp, lidar_transform)
+
+        lidar.listen(lambda data: lidar_callback(data))
+        
+        world.tick()
+        time.sleep(0.005)
+
+    finally:
+        world.apply_settings(original_settings)
+        #traffic_manager.set_synchronous_mode(False)
+
+        #vehicle.destroy()
+        lidar.destroy()
+        #vis.destroy_window()
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(
+        description=__doc__)
+    argparser.add_argument(
+        '--host',
+        metavar='H',
+        default='localhost',
+        help='IP of the host CARLA Simulator (default: localhost)')
+    argparser.add_argument(
+        '-p', '--port',
+        metavar='P',
+        default=2000,
+        type=int,
+        help='TCP port of CARLA Simulator (default: 2000)')
+    argparser.add_argument(
+        '--no-rendering',
+        action='store_true',
+        help='use the no-rendering mode which will provide some extra'
+        ' performance but you will lose the articulated objects in the'
+        ' lidar, such as pedestrians')
+    argparser.add_argument(
+        '--no-noise',
+        action='store_true',
+        help='remove the drop off and noise from the normal (non-semantic) lidar')
+    argparser.add_argument(
+        '--no-autopilot',
+        action='store_false',
+        help='disables the autopilot so the vehicle will remain stopped')
+    argparser.add_argument(
+        '--show-axis',
+        action='store_true',
+        help='show the cartesian coordinates axis')
+    argparser.add_argument(
+        '--filter',
+        metavar='PATTERN',
+        default='model3',
+        help='actor filter (default: "vehicle.*")')
+    argparser.add_argument(
+        '--upper-fov',
+        default=15.0,
+        type=float,
+        help='lidar\'s upper field of view in degrees (default: 15.0)')
+    argparser.add_argument(
+        '--lower-fov',
+        default=-25.0,
+        type=float,
+        help='lidar\'s lower field of view in degrees (default: -25.0)')
+    argparser.add_argument(
+        '--channels',
+        default=64.0,
+        type=float,
+        help='lidar\'s channel count (default: 64)')
+    argparser.add_argument(
+        '--range',
+        default=100.0,
+        type=float,
+        help='lidar\'s maximum range in meters (default: 100.0)')
+    argparser.add_argument(
+        '--points-per-second',
+        default=500000,
+        type=int,
+        help='lidar\'s points per second (default: 500000)')
+    argparser.add_argument(
+        '-x',
+        default=0.0,
+        type=float,
+        help='offset in the sensor position in the X-axis in meters (default: 0.0)')
+    argparser.add_argument(
+        '-y',
+        default=0.0,
+        type=float,
+        help='offset in the sensor position in the Y-axis in meters (default: 0.0)')
+    argparser.add_argument(
+        '-z',
+        default=1.5,
+        type=float,
+        help='offset in the sensor position in the Z-axis in meters (default: 0.0)')
+    args = argparser.parse_args()
+
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        print(' - Exited by user.')
